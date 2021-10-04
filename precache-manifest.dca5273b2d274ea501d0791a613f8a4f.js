add4e79dc2213bd2e8b93d23464fb7f4f6394613
Nguyen Tu Khanh Linh <tukhanhlinhnguyen@gmail.com>
2021-10-04 13:24:11 +0200

    first commit

---
+++ precache-manifest.dca5273b2d274ea501d0791a613f8a4f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2ae4667e02339e3904f98f93a4d2dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b2d5e1db04ce8138ac",
+    "url": "/static/css/main.c5c36485.chunk.css"
+  },
+  {
+    "revision": "e1e2456b96a16768be4a",
+    "url": "/static/js/2.f40b5258.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "/static/js/2.f40b5258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b2d5e1db04ce8138ac",
+    "url": "/static/js/main.9887099c.chunk.js"
+  },
+  {
+    "revision": "6b3d16074c7375f4124e",
+    "url": "/static/js/runtime-main.39aa26b7.js"
+  },
+  {
+    "revision": "c778feea56ae24472ff6b51602485af2",
+    "url": "/static/media/Cartoon - On _ On (feat. Daniel Levi) _NCS Release_.c778feea.mp3"
+  },
+  {
+    "revision": "795f99677f45fe21b1c2db1c0185773a",
+    "url": "/static/media/Mortals.795f9967.mp3"
+  },
+  {
+    "revision": "8aeef8cdd5ba9aba139ea5671cbccef6",
+    "url": "/static/media/mortals-feat-laura-brehm.8aeef8cd.jpg"
+  },
+  {
+    "revision": "a5b5fbaa964c56b8d55f290d711d59c7",
+    "url": "/static/media/next.a5b5fbaa.svg"
+  },
+  {
+    "revision": "f327acaf32b4f8450d754b62e45d67d3",
+    "url": "/static/media/onandon.f327acaf.jpg"
+  },
+  {
+    "revision": "b844a76af7088fe27396f38962fc82a2",
+    "url": "/static/media/pause.b844a76a.svg"
+  },
+  {
+    "revision": "c22c63b59baaad4c9fab56a9b0dbd088",
+    "url": "/static/media/play.c22c63b5.svg"
+  },
+  {
+    "revision": "b63de741124302ce689b4c4f204084d3",
+    "url": "/static/media/prev.b63de741.svg"
+  }
+]);
